bf72fbad084b1e4c598e8d6f656a84ae0554dfdf
levanjipm <myc.glace@gmail.com>
2019-08-05 22:24:36 +0700

    190805 dematerialized

--- inventory/dematerialized_dashboard.php
+++ inventory/dematerialized_dashboard.php
@@ -1,6 +1,15 @@
 <?php
 	include('inventoryheader.php');
 ?>
+<script src='../jquery-ui.js'></script>
+<link rel='stylesheet' href='../jquery-ui.css'>
+<script>
+$( function() {
+	$('#reference_dem').autocomplete({
+		source: "../codes/search_item.php"
+	 })
+});
+</script>
 <style>
 	.notification_large{
 		position:fixed;
@@ -45,7 +54,7 @@
 	.btn-x:focus{
 		outline: 0!important;
 	}
-	</style>
+</style>
 <div class='main'>
 	<h2 style='font-family:bebasneue'>Add event</h2>
 	<p><strong>Dematerialized</strong> Goods</p>
@@ -55,9 +64,9 @@
 			<label>Date</label>
 			<input type='date' class='form-control' name='date' id='date'>
 			<label>Reference to be dematerialized</label>
-			<input type='text' class='form-control' id='reference_dem' name='reference'>
+			<input type='text' class='form-control' id='reference_dem' name='reference_dem'>
 			<label>Quantity to be dematerialized</label>
-			<input type='number' class='form-control' id='quantity' name='quantity'>
+			<input type='number' class='form-control' id='quantity' name='quantity_dem'>
 			<label>Dematerialized into</label>
 			<select class='form-control' id='number_of_dem'>
 				<option value='2'>2</option>
@@ -70,6 +79,8 @@
 		</div>
 		<div id='second_form' style='display:none'>
 			<br>
+			<div id='appended_reference'>
+			</div>
 			<button type='button' class='btn btn-secondary' id='form_dem_goods_button'>Back</button>
 			<button type='button' class='btn btn-default' id='submit_dem_goods_confirmation_button'>Submit</button>
 		</div>
@@ -93,28 +104,42 @@
 		window.history.back();
 	});
 	$('#next_dem_goods_confirmation_button').click(function(){
-		$.ajax({
-			url:'../codes/check_item_availability.php',
-			data:{
-				reference : $('#reference_dem').val()
-			},
-			type:'POST',
-			success:function(response){
-				if($('date').val() == ''){
-					alert('Please insert a valid date');
-					return false;
-				} else if($('#quantity').val() == '' || $('#quantity').val() == 0){
-					alert('Please insert valid quantity');
-					return false;
-				} else if(response == 0){
-					alert('Reference not found!');
-					return false;
-				} else {
-					$('#first_form').fadeOut();
-					$('#second_form').fadeIn();
+		if($('#date').val() == ''){
+			alert('Please insert date!');
+			$('#date').focus();
+			return false;
+		} else {
+			$.ajax({
+				url:'../codes/check_item_availability.php',
+				data:{
+					reference : $('#reference_dem').val()
+				},
+				type:'POST',
+				success:function(response){
+					if($('date').val() == ''){
+						alert('Please insert a valid date');
+						return false;
+					} else if($('#quantity').val() == '' || $('#quantity').val() == 0){
+						alert('Please insert valid quantity');
+						return false;
+					} else if(response == 0){
+						alert('Reference not found!');
+						return false;
+					} else {
+						$('#appended_reference').html('');
+						$('#first_form').fadeOut();
+						var option_number = $('#number_of_dem').val();
+						for (i = 1; i<= option_number; i++){
+							$('#appended_reference').append("<div class='row'><div class='col-sm-6'><label>Reference #" + i + "</label><input type='text' class='form-control' name='reference[" + i + "]' id='reference-" + i + "' required></div><div class='col-sm-2'><label>Quantity #" + i + "</label><input type='number' class='form-control' name='quantity[" + i + "]'>");
+							$("#reference-" + i).autocomplete({
+								source: "../codes/search_item.php"
+							 });
+						}
+						$('#second_form').fadeIn();
+					}
 				}
-			}
-		});
+			});
+		}
 	});
 	var available = 1;
 	$('#submit_dem_goods_confirmation_button').click(function(){
@@ -146,6 +171,39 @@
 		});
 	})
 	$('#confirm_button').click(function(){
-		$('#found_goods_form').submit();
+		var reference_length = $('input[id^="reference-"]').length;
+		console.log(reference_length);
+		for(i = 1; i <= reference_length; i++){
+			if(i == reference_length){
+				$.ajax({
+					url:'../codes/check_item_availability.php',
+					data:{
+						reference : $('#reference-' + i).val()
+					},
+					type:'POST',
+					success:function(response){
+						console.log(response);
+						if(response != 0){
+							$('#dematerialized_form').submit();
+						}
+					}
+				});
+			} else {
+				$.ajax({
+					url:'../codes/check_item_availability.php',
+					data:{
+						reference : $('#reference-' + i).val()
+					},
+					type:'POST',
+					success:function(response){
+						console.log(response);
+						if(response == 0){
+							alert('Reference not found!');
+							return false;
+						}
+					}
+				});
+			}
+		}
 	});
 </script>
